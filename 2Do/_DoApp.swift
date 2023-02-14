119df84df537c8fe87b8125c100cf550c2a29271
Kaan Turan <kaann.dev@gmail.com>
2023-02-14 09:27:20 +0300

    Initial Commit

---
+++ 2Do/_DoApp.swift
@@ -0,0 +1,17 @@
+//
+//  _DoApp.swift
+//  2Do
+//
+//  Created by Turan, Kaan on 14.02.2023.
+//
+
+import SwiftUI
+
+@main
+struct _DoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
